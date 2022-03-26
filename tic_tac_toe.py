f42813c8d557eb6ea06fa490687a53a80b0b5d77
andrey-voter <a.v0dyany@yandex.ru>
2022-03-27 01:24:18 +0300

    Исправил все согласно Вашим замечаниям

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,64 +1,76 @@
+"""The main module"""
 class TicTac:
-
-    board = list(range(1, 10))
+    """The main class."""
+    def __init__(self):
+        self.board = list(range(1, 10))
 
     def show_board(self):
+        """This function shows board in console"""
         print("----------")
         for i in range(3):
             print(self.board[0+i*3], '|', self.board[1+i*3], '|', self.board[2+i*3])
         print("----------")
 
-    def validate_input(self, x):
-        if not x.isnumeric():
-            print('Вы ввели посторонние символы. Пожалуйста, введите натуральное число из отрезка [1, 9]')
-            return 1
-        if int(x) >= 1 and int(x) <= 9:
-            if self.board[int(x)-1] != 'X' and self.board[int(x)-1] != 'O':
-                return 0
-            else:
+    def validate_input(self, user_input):
+        """This function validates user's input"""
+        try:
+            user_input = int(user_input)
+            if 1 <= user_input <= 9:
+                if self.board[user_input-1] != 'X' and self.board[user_input-1] != 'O':
+                    return False
                 print('Данная клетка уже занята. Пожалуйста, выберите другую')
-                return 1
-        else:
-            print('Вы ввели слишком большое или слишком маленькое число. Пожалуйста, ведите число из отрезка [1, 9]')
-            return 1
+                return True
+            print('Вы ввели слишком большое или слишком маленькое число. '
+                  'Пожалуйста, ведите число из отрезка [1, 9]')
+            return True
+        except (ValueError, TypeError):
+            print('Вы ввели посторонние символы. '
+                  'Пожалуйста, введите натуральное число из отрезка [1, 9]')
+            return True
 
     def start_game(self):
-        flag = False
+        """This function starts the tic_tac_toe game"""
+        game_cycle_flag = False
+        # game_cycle_flag is a variable which is used to loop the game and to show winner in the end
         step_cnt = 0
-        while not flag:
+        while not game_cycle_flag:
             if step_cnt % 2 == 0:
                 player_token = 'X'
             else:
                 player_token = 'O'
-            print('Введите число из отрезка [1, 9], чтобы поставить' + ' ' + player_token + ' ' + 'в соответствующую клетку')
+            print('Введите число из отрезка [1, 9], чтобы поставить',
+                  player_token,
+                  'в соответствующую клетку')
             self.show_board()
-            x = input()
-            while (self.validate_input(x)):
-                x = input()
+            user_input = input()
+            while self.validate_input(user_input):
+                user_input = input()
             step_cnt += 1
-            self.board[int(x)-1] = player_token
-            flag = self.check_winner()
+            self.board[int(user_input)-1] = player_token
+            game_cycle_flag = self.check_winner()
         self.show_board()
-        if flag == 1:
-            exit(0)
-        print('Победил', flag)
+        if game_cycle_flag == 1:
+            return 0
+        print('Победил', game_cycle_flag)
 
     def check_winner(self):
-        win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+        """This function checks winner"""
+        win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8),
+                     (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
         for i in win_coord:
             if self.board[i[0]] == self.board[i[1]] == self.board[i[2]]:
                 return self.board[i[0]]
         draw_flag = 0
         for i in self.board:
-            if i != 'X' and i != 'O':
+            if i not in ('O', 'X'):
                 draw_flag = 1
                 break
         if draw_flag == 0:
             print('Игра окончена - ничья!')
-            return 1
+            return True
         return False
 
 
 if __name__ == "__main__":
-    game = TicTac()
-    game.start_game()
+    GAME = TicTac()
+    GAME.start_game()
--- tic_tac_toe_test.py
+++ tic_tac_toe_test.py
@@ -1,40 +1,35 @@
+"""The main test module"""
 import unittest
 from tic_tac_toe import TicTac
-# Test cases to test Tictac methods
 
 
-class Test_tic_tac_toe(unittest.TestCase):
-
+class TestTicTacToe(unittest.TestCase):
+    """Test cases to test Tictac methods"""
     def setUp(self):
         self.Tictac = TicTac()
 
-    # validate_input() method - given number from [1, 9],
-    # return 0 to continue the game
-    def test_validate_input_correct_number(self):
+    def test_validate_input(self):
+        """Test cases to test different input variants"""
         self.Tictac.board = list(range(1, 10))
-        self.assertEqual(self.Tictac.validate_input('1'), 0)
 
-    # validate_input() method - given number not from [1, 9],
-    # return 1 to ask player to input again
-    def test_validate_input_incorrect_number(self):
-        self.assertEqual(self.Tictac.validate_input('100'), 1)
+        # return false to continue the game
+        self.assertFalse(self.Tictac.validate_input('1'))
 
-    # validate_input() method - given not a number,
-    # return 1 to ask player to input again
-    def test_validate_input_not_number(self):
-        self.assertEqual(self.Tictac.validate_input('aaa'), 1)
+        # return true to ask for new input
+        self.assertTrue(self.Tictac.validate_input('100'))
 
-    # validate_input() method - given a number from [1, 9],
-    # but the cell is already busy
-    # return 1 to ask player to input again
-    def test_validate_input_busy_cell(self):
+        # return true to ask for new input
+        self.assertTrue(self.Tictac.validate_input('aaa'))
+
         self.Tictac.board[int('2') - 1] = 'X'
-        self.assertEqual(self.Tictac.validate_input('2'), 1)
+        # return true to ask for new input
+        self.assertTrue(self.Tictac.validate_input('2'))
         self.Tictac.board = list(range(1, 10))
 
-    # check_winner() method - all cells are busy and there is no winner
-    # - then it is a draw, return 1 to stop the game
-    def test_check_winner_draw(self):
+    def test_check_winner_x(self):
+        """Test cases to test different variants of draw and x winning"""
+        # check_winner() method - all cells are busy and there is no winner
+        # - then it is a draw, return True to stop the game
         for i in range(6):
             if i % 2 == 0:
                 self.Tictac.board[i] = 'X'
@@ -45,77 +40,148 @@ class Test_tic_tac_toe(unittest.TestCase):
                 self.Tictac.board[i] = 'O'
             else:
                 self.Tictac.board[i] = 'X'
-        self.assertEqual(self.Tictac.check_winner(), 1)
+        self.assertTrue(self.Tictac.check_winner())
         self.Tictac.board = list(range(1, 10))
 
-    # check_winner() method - all cells are free -
-    # then the game has just started!
-    # return 0 to continue the game
-    def test_check_winner0(self):
-        self.assertEqual(self.Tictac.check_winner(), 0)
+        # check_winner() method - all cells are free -
+        # then the game has just started!
+        # return False to continue the game
+        self.assertFalse(self.Tictac.check_winner())
+        self.Tictac.board = list(range(1, 10))
 
-    # check_winner() method - 'X' has won the first way,
-    # return 'X' to stop the game and show the winner
-    def test_check_winner1(self):
+        # check_winner() method - 'X' has won the first way,
+        # return 'X' to stop the game and show the winner
         for i in (0, 1, 2):
             self.Tictac.board[i] = 'X'
         self.assertEqual(self.Tictac.check_winner(), 'X')
         self.Tictac.board = list(range(1, 10))
 
-    # check_winner() method - 'X' has won the second way,
-    # return 'X' to stop the game and show the winner
-    def test_check_winner2(self):
+        # check_winner() method - 'X' has won the second way,
+        # return 'X' to stop the game and show the winner
         for i in (3, 4, 5):
             self.Tictac.board[i] = 'X'
         self.assertEqual(self.Tictac.check_winner(), 'X')
         self.Tictac.board = list(range(1, 10))
 
-    # check_winner() method - 'X' has won the third way,
-    # return 'X' to stop the game and show the winner
-    def test_check_winner3(self):
+        # check_winner() method - 'X' has won the third way,
+        # return 'X' to stop the game and show the winner
         for i in (6, 7, 8):
             self.Tictac.board[i] = 'X'
         self.assertEqual(self.Tictac.check_winner(), 'X')
         self.Tictac.board = list(range(1, 10))
 
-    # check_winner() method - 'X' has won the fourth way,
-    # return 'X' to stop the game and show the winner
-    def test_check_winner4(self):
+        # check_winner() method - 'X' has won the fourth way,
+        # return 'X' to stop the game and show the winner
         for i in (0, 3, 6):
             self.Tictac.board[i] = 'X'
         self.assertEqual(self.Tictac.check_winner(), 'X')
         self.Tictac.board = list(range(1, 10))
 
-    # check_winner() method - 'X' has won the fifth way,
-    # return 'X' to stop the game and show the winner
-    def test_check_winner5(self):
+        # check_winner() method - 'X' has won the fifth way,
+        # return 'X' to stop the game and show the winner
         for i in (1, 4, 7):
             self.Tictac.board[i] = 'X'
         self.assertEqual(self.Tictac.check_winner(), 'X')
         self.Tictac.board = list(range(1, 10))
 
-    # check_winner() method - 'X' has won the sixth way,
-    # return 'X' to stop the game and show the winner
-    def test_check_winner6(self):
+        # check_winner() method - 'X' has won the sixth way,
+        # return 'X' to stop the game and show the winner
         for i in (2, 5, 8):
             self.Tictac.board[i] = 'X'
         self.assertEqual(self.Tictac.check_winner(), 'X')
         self.Tictac.board = list(range(1, 10))
 
-    # check_winner() method - 'X' has won the seventh way,
-    # return 'X' to stop the game and show the winner
-    def test_check_winner7(self):
+        # check_winner() method - 'X' has won the seventh way,
+        # return 'X' to stop the game and show the winner
         for i in (0, 4, 8):
             self.Tictac.board[i] = 'X'
         self.assertEqual(self.Tictac.check_winner(), 'X')
         self.Tictac.board = list(range(1, 10))
 
-    # check_winner() method - 'X' has won the eighth way,
-    # return 'X' to stop the game and show the winner
-    def test_check_winner8(self):
+        # check_winner() method - 'X' has won the eighth way,
+        # return 'X' to stop the game and show the winner
         for i in (2, 4, 6):
             self.Tictac.board[i] = 'X'
         self.assertEqual(self.Tictac.check_winner(), 'X')
+        self.Tictac.board = list(range(1, 10))
+
+    def test_check_winner_o(self):
+        """Test cases to test different variants of draw and O winning"""
+        # check_winner() method - all cells are busy and there is no winner
+        # - then it is a draw, return True to stop the game
+        for i in range(6):
+            if i % 2 == 0:
+                self.Tictac.board[i] = 'O'
+            else:
+                self.Tictac.board[i] = 'X'
+        for i in range(6, 9):
+            if i % 2 == 0:
+                self.Tictac.board[i] = 'X'
+            else:
+                self.Tictac.board[i] = 'O'
+        self.assertTrue(self.Tictac.check_winner())
+        self.Tictac.board = list(range(1, 10))
+
+        # check_winner() method - all cells are free -
+        # then the game has just started!
+        # return False to continue the game
+        self.assertFalse(self.Tictac.check_winner())
+        self.Tictac.board = list(range(1, 10))
+
+        # check_winner() method - 'O' has won the first way,
+        # return 'X' to stop the game and show the winner
+        for i in (0, 1, 2):
+            self.Tictac.board[i] = 'O'
+        self.assertEqual(self.Tictac.check_winner(), 'O')
+        self.Tictac.board = list(range(1, 10))
+
+        # check_winner() method - 'O' has won the second way,
+        # return 'O' to stop the game and show the winner
+        for i in (3, 4, 5):
+            self.Tictac.board[i] = 'O'
+        self.assertEqual(self.Tictac.check_winner(), 'O')
+        self.Tictac.board = list(range(1, 10))
+
+        # check_winner() method - 'O' has won the third way,
+        # return 'O' to stop the game and show the winner
+        for i in (6, 7, 8):
+            self.Tictac.board[i] = 'O'
+        self.assertEqual(self.Tictac.check_winner(), 'O')
+        self.Tictac.board = list(range(1, 10))
+
+        # check_winner() method - 'O' has won the fourth way,
+        # return 'O' to stop the game and show the winner
+        for i in (0, 3, 6):
+            self.Tictac.board[i] = 'O'
+        self.assertEqual(self.Tictac.check_winner(), 'O')
+        self.Tictac.board = list(range(1, 10))
+
+        # check_winner() method - 'O' has won the fifth way,
+        # return 'O' to stop the game and show the winner
+        for i in (1, 4, 7):
+            self.Tictac.board[i] = 'O'
+        self.assertEqual(self.Tictac.check_winner(), 'O')
+        self.Tictac.board = list(range(1, 10))
+
+        # check_winner() method - 'O' has won the sixth way,
+        # return 'O' to stop the game and show the winner
+        for i in (2, 5, 8):
+            self.Tictac.board[i] = 'O'
+        self.assertEqual(self.Tictac.check_winner(), 'O')
+        self.Tictac.board = list(range(1, 10))
+
+        # check_winner() method - 'O' has won the seventh way,
+        # return 'O' to stop the game and show the winner
+        for i in (0, 4, 8):
+            self.Tictac.board[i] = 'O'
+        self.assertEqual(self.Tictac.check_winner(), 'O')
+        self.Tictac.board = list(range(1, 10))
+
+        # check_winner() method - 'X' has won the eighth way,
+        # return 'X' to stop the game and show the winner
+        for i in (2, 4, 6):
+            self.Tictac.board[i] = 'O'
+        self.assertEqual(self.Tictac.check_winner(), 'O')
         self.Tictac.board = list(range(1, 10))
 
 
