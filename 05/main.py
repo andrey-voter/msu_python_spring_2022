7720cbc470d6971f27aec56609a2ca2f4badcb8f
andrey-voter <a.v0dyany@yandex.ru>
2022-05-05 01:29:06 +0300

    lru cache

--- 05/main.py
+++ 05/main.py
@@ -7,21 +7,13 @@ if __name__ == '__main__':
 
     cache.set("k1", "val1")
     cache.set("k2", "val2")
+
+    print(cache.get("k3"))  # None
+    print(cache.get("k2"))  # "val2"
+    print(cache.get("k1"))  # "val1"
+
     cache.set("k3", "val3")
-    print(cache.cache, cache.box)
-    cache.set("k3", "val33")
-    print(cache.cache, cache.box)
-    # print(cache.get("k1"))
-    # print(cache.get("k2"))
-    # print(cache.get("k3"))
 
-
-    # print(cache.get("k3"))  # None
-    # print(cache.get("k2"))  # "val2"
-    # print(cache.get("k1"))  # "val1"
-    #
-    # cache.set("k3", "val3")
-    #
-    # print(cache.get("k3"))  # "val3"
-    # print(cache.get("k2"))  # None
-    # print(cache.get("k1"))  # "val1"
+    print(cache.get("k3"))  # "val3"
+    print(cache.get("k2"))  # None
+    print(cache.get("k1"))  # "val1"
